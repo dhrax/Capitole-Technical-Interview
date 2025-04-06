9d7530d1bf389dff19c2cd031a32b9b89a085a69
david <davidfdez006@gmail.com>
2025-04-06 14:01:28 +0200

    Pagination implemented for Product API

--- src/main/kotlin/com/example/capitoleInterview/controller/ProductController.kt
+++ src/main/kotlin/com/example/capitoleInterview/controller/ProductController.kt
@@ -2,6 +2,7 @@ package com.example.capitoleInterview.controller
 
 import com.example.capitoleInterview.dto.ProductDTO
 import com.example.capitoleInterview.service.ProductService
+import org.hibernate.annotations.Parameter
 import org.springframework.data.domain.Page
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -16,9 +17,11 @@ class ProductController(
 
     @GetMapping
     fun listProducts(
+        @RequestParam(defaultValue = "0") page: Int,
+        @RequestParam(defaultValue = "5") size: Int,
         @RequestParam(required = false) category: String?,
-        @RequestParam(required = false) sortBy: String?
-    ): List<ProductDTO> {
-        return productService.getProducts(category, sortBy)
+        @RequestParam(required = false)  sortBy: String?
+    ): Page<ProductDTO> {
+        return productService.getProducts(page, size, category, sortBy)
     }
 }
